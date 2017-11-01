8454e4a03fd1b89910f1439e82597fcf270c55ac
jcn13 <jcn13@uol.com.br>
2017-11-01 11:31:31 -0700

    more adjustments

--- read.js
+++
@@ -1,8 +0,0 @@
-const firebase = require('firebase')
-
-firebase.initializeApp({
-	databaseURL: 'https://test-eb50e.firebaseio.com/'
-})
-const dbRef = firebase.database().ref('people/')
-
-dbRef.on('value', data => console.log(data.val()))
